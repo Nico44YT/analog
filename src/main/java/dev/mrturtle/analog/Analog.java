01af77c0011b6badad8f197c61069657b9080709
MrTurtle <66325227+Gibatron@users.noreply.github.com>
2024-07-28 15:09:25 -0400

    Remove unneeded potentially problematic code, bumps version to v1.1.1

--- src/main/java/dev/mrturtle/analog/Analog.java
+++ src/main/java/dev/mrturtle/analog/Analog.java
@@ -1,13 +1,9 @@
 package dev.mrturtle.analog;
 
-import de.maxhenkel.voicechat.api.opus.OpusDecoder;
-import de.maxhenkel.voicechat.api.opus.OpusEncoder;
 import dev.mrturtle.analog.config.ConfigManager;
-import dev.mrturtle.analog.util.RadioUtil;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,14 +20,15 @@ public class Analog implements ModInitializer {
 		ModBlocks.initialize();
 		ModBlockEntities.initialize();
 		ModSounds.initialize();
+		// Encoders aren't being used anymore
 		// Clean up player's encoder and decoder on disconnect
-		ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
+		/*ServerPlayConnectionEvents.DISCONNECT.register((handler, server) -> {
 			OpusEncoder encoder = RadioUtil.playerEncoders.remove(handler.player.getUuid());
 			if (encoder != null)
 				encoder.close();
 			OpusDecoder decoder = RadioUtil.playerDecoders.remove(handler.player.getUuid());
 			if (decoder != null)
 				decoder.close();
-		});
+		});*/
 	}
 }
